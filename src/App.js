8c73f3376ea5844876b82deaebe518abaf2951de
GSometimes <32429409+GSometimes@users.noreply.github.com>
2020-11-29 12:58:35 -0800

    Rendered Contentful Data

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./styles.css";
 import Circles from "./components/circles";
 import { Switch, Route, Link } from "react-router-dom";
+import Test from "./components/test"
 
 export default function App() {
   return (
@@ -29,6 +30,7 @@ export default function App() {
           render={(routerProps) => <Circles {...routerProps} />}
         />
       </Switch>
+      <Test/>
     </div>
   );
 }
---
+++ src/components/client.js
@@ -0,0 +1,7 @@
+import * as contentful from "contentful"
+
+
+export const client = contentful.createClient({
+  space: process.env.REACT_APP_SPACE_ID,
+  accessToken: process.env.REACT_APP_ACCESS_TOKEN
+})
